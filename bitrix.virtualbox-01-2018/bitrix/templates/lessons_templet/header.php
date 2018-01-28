51dbe6308ea6a092ee1729f0d384d471a36880ba
tvmichael <tvmichael@meta.ua>
2018-01-28 14:33:29 +0200

    work components + comment

--- bitrix.virtualbox-01-2018/bitrix/templates/lessons_templet/header.php
+++ bitrix.virtualbox-01-2018/bitrix/templates/lessons_templet/header.php
@@ -11,29 +11,19 @@ $curPage = $APPLICATION->GetCurPage(true);
 	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
 	<link rel="shortcut icon" type="image/x-icon" href="<?=SITE_DIR?>favicon.ico" />
 	<?$APPLICATION->ShowHead();?>
-
     <?
 	$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH."/colors.css", true);
 	$APPLICATION->SetAdditionalCSS("/bitrix/css/main/bootstrap.css");
 	$APPLICATION->SetAdditionalCSS("/bitrix/css/main/font-awesome.css");
-
-    //$APPLICATION->SetAdditionalCSS("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css");
-	//$APPLICATION->AddHeadScript('https://code.jquery.com/jquery-3.1.1.slim.min.js');
-    //$APPLICATION->AddHeadScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js');
+    $APPLICATION->AddHeadScript('https://code.jquery.com/jquery-3.1.1.slim.min.js');
 	?>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
     <title><?$APPLICATION->ShowTitle()?></title>
 </head>
 
 <body class="bx-background-image">
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col-md-12">
             <div>
