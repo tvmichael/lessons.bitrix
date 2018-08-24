85a421deb642f7ea61385e4643a1bbe388b6dded
tvmichael <tvmichael@meta.ua>
2018-08-24 21:07:13 +0300

    badges

--- bitrix.virtualbox-07-2018/bitrix/components/mv/badge.check/templates/.default/template.php
+++ bitrix.virtualbox-07-2018/bitrix/components/mv/badge.check/templates/.default/template.php
@@ -1,5 +1,8 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
+use Bitrix\Main\IO,
+    Bitrix\Main\Application;
+
 /**
  * @global CMain $APPLICATION
  * @var array $arParams
@@ -10,76 +13,129 @@
  * @var string $componentPath
  * @var string $templateFolder
  */
- ?>
+?>
 
-<div class="bx-badge-container">
-	<div class="bx-badge-inner">
-		<?if($arParams['SHOW_BADGES'] == 'Y'):?>
+    <div class="bx-badge-container">
+        <div class="bx-badge-inner">
+            <?if($arParams['SHOW_BADGES'] == 'Y'):?>
 
-			<?
-			// DELIVERY
-			if ( count($arResult['DELIVERY']) > 0 && $arParams['SHOW_BADGES_DELIVERY'] = 'Y' ):
-			?>
-				<div class='bx-badge-delivery'>
-					<img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_DELIVERY_IMG']);?>'>
-				</div>
-			<? endif; ?>
+                <? // DELIVERY
+                if ( count($arResult['DELIVERY']) > 0 && $arParams['SHOW_BADGES_DELIVERY'] == 'Y' ):
 
-			<?
-			// CERTIFICATE
-			if ( count($arResult['CERTIFICATE']) >0 && $arParams['SHOW_BADGES_CERTIFICATE'] = 'Y' ):
-			?>
-				<div class='bx-badge-certificate'>
-					<img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_CERTIFICATE_IMG']);?>'>
-				</div>
-			<? endif; ?>
+                    $showBadge = false;
+                    foreach ($arResult['DELIVERY'] as $value) {
+                        if( $value['TIMER'] >= 1)  $showBadge = true;
+                    }
+                    if($showBadge){
+                        ?>
+                        <div class='bx-badge-delivery'>
+                            <img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_DELIVERY_IMG']);?>'>
+                        </div>
+                    <?};
 
-			<?
-			// STOCK
-			if ( count($arResult['STOCK']) > 0 && $arParams['SHOW_BADGES_STOCK'] = 'Y' ):
-			?>
-				<div class='bx-badge-stock'>
-					<img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_STOCK_IMG']);?>'>
-				</div>
-			<? endif; ?>
-
-			<?
-			// DISCOUNT
-			if ( count($arResult['DISCOUNT']) > 0 && $arParams['SHOW_BADGES_DISCOUNT'] = 'Y' ):
-			?>
-				<div class='bx-badge-discount'>
-					<img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_DISCOUNT_IMG']);?>'>
-				</div>
-			<? endif;?>
-
-			<?
-			// GIFT
-			if ( count($arResult['GIFT']) > 0 && $arParams['SHOW_BADGES_GIFT'] = 'Y'):
-			?>
-				<div class='bx-badge-gift'>
-					<img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_GIFT_IMG']);?>'>
-				</div>
-			<? endif;?>
-
-		<?endif;?>
-	</div>
-</div>
+                endif;
+                ?>
 
 
+                <? // CERTIFICATE - купон
+                if ( count($arResult['CERTIFICATE']) > 0 && $arParams['SHOW_BADGES_CERTIFICATE'] == 'Y' ):
+                    $images = $arParams['SHOW_BADGES_CERTIFICATE_IMG'];
+                    $price = 0;
+                    $showBadge = false;
+
+                    foreach ($arResult['CERTIFICATE'] as $value) {
+                        if (isset($value['DATA']['SHORT_DESCRIPTION_STRUCTURE']['VALUE']))
+                            if( $value['DATA']['SHORT_DESCRIPTION_STRUCTURE']['VALUE'] > $price && $value['DATA']['SHORT_DESCRIPTION_STRUCTURE']['VALUE_TYPE']=='F' )
+                                $price = $value['DATA']['SHORT_DESCRIPTION_STRUCTURE']['VALUE'];
+                        //if( $value['TIMER'] > date('Y-m-d H:i:s') ) $showCertificate = true;
+                        if( $value['TIMER'] != 0 && ($value['TIMER'] > time() || $value['TIMER'] == 1) ) $showBadge = true;
+                        //echo "<h1>".$value['TIMER'].'>='.time().'<h1><hr>';
+                    }
+
+                    if ($price > 0 && $showBadge):
+                        $images = explode('.', $images);
+                        if (count($images) == 2){
+                            $images = $images[0].$price.'.'.$images[1];
+                        }
+                        $file = $templateFolder.'/images/'.$images;
+                        ?>
+
+                        <div class='bx-badge-certificate'>
+                            <img src='<?=$file;?>'>
+                        </div>
+                    <? endif; ?>
+                <? endif; ?>
+
+
+
+                <? // STOCK
+                if ( count($arResult['STOCK']) > 0 && $arParams['SHOW_BADGES_STOCK'] == 'Y' ):
+
+                    $showBadge = false;
+                    foreach ($arResult['STOCK'] as $value) {
+                        if( $value['TIMER'] >= 1)  $showBadge = true;
+                    }
+                    if($showBadge){
+                        ?>
+                        <div class='bx-badge-stock'>
+                            <img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_STOCK_IMG']);?>'>
+                        </div>
+                    <?}
+                endif; ?>
+
+                <?
+                // DISCOUNT
+                if ( count($arResult['DISCOUNT']) > 0 && $arParams['SHOW_BADGES_DISCOUNT'] == 'Y' ):
+
+                    $showBadge = false;
+                    foreach ($arResult['DISCOUNT'] as $value) {
+                        if( $value['TIMER'] >= 1)  $showBadge = true;
+                    }
+                    if($showBadge){
+                        ?>
+                        <div class='bx-badge-discount'>
+                            <img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_DISCOUNT_IMG']);?>'>
+                        </div>
+                    <?}
+                endif;?>
+
+                <?
+                // GIFT
+                if ( count($arResult['GIFT']) > 0 && $arParams['SHOW_BADGES_GIFT'] == 'Y'):
+
+                    $showBadge = false;
+                    foreach ($arResult['GIFT'] as $value) {
+                        if( $value['TIMER'] >= 1)  $showBadge = true;
+                    }
+                    if($showBadge){
+                        ?>
+                        <div class='bx-badge-gift'>
+                            <img src='<?=($templateFolder.'/images/'.$arParams['SHOW_BADGES_GIFT_IMG']);?>'>
+                        </div>
+                    <?}
+                endif;?>
+
+            <?endif;?>
+        </div>
+    </div>
 
 
 
 
 <?
-if ( $USER->IsAdmin() )
+if ( $USER->IsAdmin() && $USER->GetID() == 106 && false )
 {?>
-<pre style="position: absolute; z-index: 500;">
+    <pre style="position: absolute; z-index: 500; top: 650px;">
 <?
-echo "Params:<br>";
-print_r($arParams);
-echo "<hr>";
-echo "Result:<br>";
-print_r($arResult);
+if (!isset($GLOBALS['BADGE_PARAM_TIMER_ON_XXX'])) {
+
+    $GLOBALS['BADGE_PARAM_TIMER_ON_XXX'] = 'XXX';
+    echo "Params:<br>";
+    print_r($arParams);
+    echo "<hr>";
+    echo "Result:<br>";
+    print_r($arResult);
+}
 ?>
 </pre>
 <?};
